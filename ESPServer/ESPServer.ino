95be0586db548c7efbc5330e71c25a334e58cd1c
StevenProgrammer01 <steven.josspa@gmail.com>
2025-02-06 11:23:19 -0600

    Codigo Server y ESP para envio y recepcion de datos

---
+++ ESPServer/ESPServer.ino
@@ -0,0 +1,175 @@
+#include <WiFi.h>
+#include "DFRobot_MultiGasSensor.h"
+#include <Wire.h>
+#include "DHTesp.h"
+#include <WiFiUdp.h>
+#include <ArduinoJson.h>
+DHTesp dht;
+#define DHTPIN 12     // Digital pin connected to the DHT sensor (replace with your pin number)
+#define DHTTYPE DHT22
+
+#if defined(ESP32) || defined(ESP8266)
+  // D7 pin is used as interrupt pin by default, other non-conflicting pins can also be selected as external interrupt pins.
+  #define SENSOR_DATA_PIN   (7)   // Sensor PWM interface
+  #define INTERRUPT_NUMBER   digitalPinToInterrupt(SENSOR_DATA_PIN)   // interrupt number
+#elif defined(ARDUINO_SAM_ZERO)
+  // Pin 5 is used as interrupt pin by default, other non-conflicting pins can also be selected as external interrupt pins
+  #define SENSOR_DATA_PIN   (5)   // Sensor PWM interface
+  #define INTERRUPT_NUMBER   digitalPinToInterrupt(SENSOR_DATA_PIN)   // interrupt number
+#else
+  // Open the external interrupt 0, connect INT1/2 to the digital pin of the main control: 
+  // UNO(2), Mega2560(2), Leonardo(3), microbit(P0).
+  #define SENSOR_DATA_PIN   (2)   // Sensor PWM interface
+  #define INTERRUPT_NUMBER   (0)   // interrupt number
+#endif
+
+// Used in interrupt, calculate pulse width variable
+volatile unsigned long pwmHighStartTicks=0, pwmHighEndTicks=0;
+volatile unsigned long pwmHighVal=0, pwmLowVal=0;
+// interrupt flag
+volatile uint8_t flag=0;
+
+//Enabled by default, use IIC communication at this time. Use UART communication when disabled
+#define I2C_COMMUNICATION
+
+#ifdef  I2C_COMMUNICATION
+#define I2C_ADDRESS    0x74
+DFRobot_GAS_I2C gas(&Wire ,I2C_ADDRESS);
+#else
+#if (!defined ARDUINO_ESP32_DEV) && (!defined __SAMD21G18A__)
+  SoftwareSerial mySerial(0,1);
+  DFRobot_GAS_SoftWareUart gas(&mySerial);
+#else
+  DFRobot_GAS_HardWareUart gas(&Serial1,9600,15,16); //ESP32HardwareSerial
+#endif
+#endif
+
+const char* ssid = "Steven"; 
+const char* password = "steven123";
+const char* serverIP = "192.168.107.129";  // IP de tu PC
+const int serverPort = 8000;
+void interruptChange()
+
+{
+  if (digitalRead(SENSOR_DATA_PIN)) {
+    pwmHighStartTicks = micros();    // store the current micros() value
+    if(2 == flag){
+      flag = 4;
+      if(pwmHighStartTicks > pwmHighEndTicks) {
+        pwmLowVal = pwmHighStartTicks - pwmHighEndTicks;
+      }
+    }else{
+      flag = 1;
+    }
+  } else {
+    pwmHighEndTicks = micros();    // store the current micros() value
+    if(1 == flag){
+      flag = 2;
+      if(pwmHighEndTicks > pwmHighStartTicks){
+        pwmHighVal = pwmHighEndTicks - pwmHighStartTicks;
+      }
+    }
+  }
+}
+WiFiUDP udp;
+void setup() {
+  Serial.begin(115200);
+  dht.setup(DHTPIN, DHTesp::DHT22);
+  // Inicia la conexión a la red WiFi
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.println("Conectando a la red WiFi...");
+  }
+
+  Serial.println("Conectado a la red WiFi");
+  
+
+  Wire.begin(16, 11);
+
+  while(!gas.begin())
+  {
+    Serial.println("No Devices !");
+    delay(1000);
+  }
+  Serial.println("The device is connected successfully!");
+
+  gas.changeAcquireMode(gas.PASSIVITY);
+  delay(1000);
+
+  gas.setTempCompensation(gas.OFF);
+
+  pinMode(SENSOR_DATA_PIN, INPUT);
+  attachInterrupt(INTERRUPT_NUMBER, interruptChange, CHANGE);
+  Serial.println("3 min for beginning...");
+
+}
+
+
+
+
+void loop() {
+
+  StaticJsonDocument<200> doc;
+  char buffer[128];
+
+  float concentration;
+  //WiFiClient client = server.available(); // Escucha por clientes entrantes
+  
+    
+  Serial.println("Esperando datos...");
+  delay(10000);
+  float temp = dht.getTemperature();
+  float humidity = dht.getHumidity();
+  Serial.print("Temperature: ");Serial.println(temp);
+  Serial.print("Humidity: ");Serial.println(humidity);
+  Serial.println(flag);
+  if(flag == 4){
+  flag = 1;
+  float pwmHighVal_ms = (pwmHighVal * 1000.0) / (pwmLowVal + pwmHighVal);
+
+  if (pwmHighVal_ms < 0.01){
+    Serial.println("Fault");
+  }
+  else if (pwmHighVal_ms < 80.00){
+    Serial.println("preheating");
+  }
+  else if (pwmHighVal_ms < 998.00){
+    concentration = (pwmHighVal_ms - 2) * 5;
+    // Print pwmHighVal_ms
+    Serial.print("pwmHighVal_ms:");
+    Serial.print(pwmHighVal_ms);
+    Serial.println("ms");
+    //Print CO2 concentration
+    Serial.print(concentration);
+    Serial.println("ppm");
+  }else{
+    Serial.println("Beyond the maximum range : 398~4980ppm");
+  }
+  Serial.println();
+}
+  Serial.print("Ambient ");
+  Serial.print(gas.queryGasType());
+  Serial.print(" concentration is: ");
+  Serial.print(gas.readGasConcentrationPPM());
+  Serial.println(" %vol");
+
+  doc["o2"] =  gas.readGasConcentrationPPM();
+  doc["co2"] = concentration ;
+  doc["temperature"] = temp;
+  doc["humidity"] = humidity;
+
+  //cadena = String(gas.readGasConcentrationPPM())+"@"+String(concentration)+"@"+String(temp)+"@"+String(humidity);
+
+  size_t len = serializeJson(doc, buffer);
+  if (concentration > 0){
+    udp.beginPacket(serverIP, serverPort);
+    udp.write((uint8_t*)buffer, len);
+    udp.endPacket();
+    Serial.println("Datos enviados: " + String(buffer));
+  }
+  
+      
+}
+
---
+++ ServerCreatus/MainWindow.xaml.cs
@@ -0,0 +1,120 @@
+﻿using LiveCharts;
+using LiveCharts.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+using System.Windows;
+using System.ComponentModel;
+using System.Windows.Media;
+
+namespace ServerCreatus
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private ChartValues<double> _co2;
+        private ChartValues<double> _o2;
+
+        public ChartValues<double> co2
+        {
+            get => _co2;
+            set { _co2 = value; OnPropertyChanged(nameof(co2)); }
+        }
+
+        public ChartValues<double> o2
+        {
+            get => _o2;
+            set { _o2 = value; OnPropertyChanged(nameof(o2)); }
+        }
+
+        public MainViewModel()
+        {
+            co2 = new ChartValues<double> { 400, 450, 480, 500 };
+            o2 = new ChartValues<double> { 21, 20.8, 20.5, 20.2 };
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        public ChartValues<double> co2Values { get; set; }
+        public ChartValues<double> o2Values { get; set; }
+
+        private UdpClient _udpServer;
+        private const int PORT = 8000;
+
+        public MainWindow()
+        {
+            
+
+            InitializeComponent();
+            DataContext = new MainViewModel();
+
+            co2Values = new ChartValues<double>();
+            o2Values = new ChartValues<double>();
+
+            CO2Chart.Series = new SeriesCollection
+            {
+                new LineSeries { Title = "CO2 ppm", Values = co2Values,Stroke = new SolidColorBrush(Color.FromRgb(255, 0, 0)), // Rojo puro
+        Fill = Brushes.Transparent, }
+
+            };
+
+            O2Chart.Series = new SeriesCollection
+            {
+                new LineSeries { Title = "O2 0-25%", Values = o2Values }
+            };
+
+            Task.Run(ReceiveData);
+        }
+
+        private async Task ReceiveData()
+        {
+            _udpServer = new UdpClient(PORT);
+            while (true)
+            {
+                var results = await _udpServer.ReceiveAsync();
+                string data = Encoding.UTF8.GetString(results.Buffer);
+
+                try
+                {
+                    var sensorData = JsonConvert.DeserializeObject<SensorData>(data);
+                    Console.WriteLine("Datos recibidos: " + JsonConvert.SerializeObject(sensorData));
+                    Console.WriteLine(sensorData.co2);
+                    Console.WriteLine(sensorData.o2);
+                    Dispatcher.Invoke(() =>
+                    {
+                        co2Values.Add(sensorData.co2);
+                        o2Values.Add(sensorData.o2);
+
+                        // Notificar a la UI que los datos han cambiado
+                        OnPropertyChanged(nameof(co2Values));
+                        OnPropertyChanged(nameof(o2Values));
+                    });
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error al procesar datos: " + ex.Message);
+                }
+            }
+        }
+
+        public class SensorData
+        {
+            public double co2 { get; set; }
+            public double o2 { get; set; }
+            public double temperature { get; set; }
+            public double humidity { get; set; }
+        }
+    }
+}
